8d22a386a54b183cd31aef114c2df3bdb45d7b41
GBRJo <anchernyakova@yandex.ru>
2024-02-11 02:25:54 +0400

    feat: add 0-48

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -606,8 +606,23 @@ function shiftArray(arr, n) {
  *   sortDigitNamesByNumericOrder([ 'nine','eight','nine','eight' ]) => [ 'eight','eight','nine','nine']
  *   sortDigitNamesByNumericOrder([ 'one','one','one','zero' ]) => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const numbers = {
+    zero: 0,
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
+
+  arr.sort((a, b) => numbers[a] - numbers[b]);
+
+  return arr;
 }
 
 /**
@@ -629,8 +644,19 @@ function sortDigitNamesByNumericOrder(/* arr */) {
  *   swapHeadAndTail([]) => []
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  if (arr.length % 2 === 0) {
+    const middle = arr.length / 2;
+    const first = arr.slice(0, middle);
+    const second = arr.slice(middle);
+    return second.concat(first);
+  }
+
+  const middle = Math.floor(arr.length / 2);
+  const first = arr.slice(0, middle);
+  const second = arr.slice(middle + 1);
+  second.push(arr[middle]);
+  return second.concat(first);
 }
 
 module.exports = {
